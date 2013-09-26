e90308971c02cbd5f50c45629c56da3ec6eb581c
Neeraj Khede <neeraj.khede@ideavate.com>
2013-09-26 22:45:09 +0530

    commit

--- GitGit/GitGit/ALAppDelegate.m
+++ GitGit/GitGit/ALAppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSLog(@"");
+    NSLog(@"hello");
     // Override point for customization after application launch.  
     return YES;
 }
